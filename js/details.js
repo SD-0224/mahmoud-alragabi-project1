1e87c5c8c0cae2440bbc5499ee66d14a813d05b1
MedoAngel <actionman14.ma@gmail.com>
2024-02-23 17:41:22 +0100

    add details html and js

---
+++ js/details.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const favourites = topics.filter((topic) => topic.isFavourite);
+  const favouritesContainer = document.getElementById("favourites-container");
+  const favouriteCardsElements = favourites.map(createFavouriteCardElement);
+
+  appendElementsToContainer(favouritesContainer, favouriteCardsElements);
+});
