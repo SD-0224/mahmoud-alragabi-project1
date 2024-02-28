567eed9cd8fcdc427f8a29145a457a0caefeaf5e
MedoAngel <actionman14.ma@gmail.com>
2024-02-28 15:45:19 +0100

    add search ability

--- js/index.js
+++ js/index.js
@@ -8,26 +8,71 @@ import {
 } from "./handlers/navbar-handlers.js";
 import { appendElementsToContainer } from "./modules/dom-manipulation.js";
 
+const fetchTopics = async function (endPoint) {
+  const topicsResponse = await fetch(endPoint);
+
+  const topics = await topicsResponse.json();
+
+  return topics;
+};
+
+const renderTopics = function (topics = []) {
+  const searchResultsHeader = document.getElementById("search-results");
+
+  searchResultsHeader.innerText = `"${topics.length}" Web Topics Found`;
+
+  const mainCardsContainer = document.getElementById("cards-container");
+  const mainCardsElements = topics.map(createMainCardElement);
+
+  mainCardsContainer.innerHTML = "";
+
+  appendElementsToContainer(mainCardsContainer, mainCardsElements);
+};
+
+const fetchTopicsBySearchTerm = async function (term = "") {
+  const searchEndpoint = `https://tap-web-1.herokuapp.com/topics/list?phrase=${term}`;
+
+  const topics = await fetchTopics(searchEndpoint);
+
+  return topics;
+};
+
+const debounce = function (func, timeout = 300) {
+  let timer;
+
+  return (...args) => {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+};
+
+const searchEventHandler = async (event) => {
+  const searchTerm = event.target.value.trim();
+
+  const topics = await fetchTopicsBySearchTerm(searchTerm);
+
+  renderTopics(topics);
+};
+
 document.addEventListener("DOMContentLoaded", async function () {
   let topics = [];
 
   try {
     const topicsEndpoint = "https://tap-web-1.herokuapp.com/topics/list";
-    const topicsResponse = await fetch(topicsEndpoint);
 
-    topics = await topicsResponse.json();
+    topics = await fetchTopics(topicsEndpoint);
+
+    renderTopics(topics);
   } catch (error) {
     console.log(error);
   }
 
-  const mainCardsContainer = document.getElementById("cards-container");
-  const mainCardsElements = topics.map(createMainCardElement);
-
   const favourites = topics.filter((topic) => topic.isFavourite);
   const favouritesContainer = document.getElementById("favourites-container");
   const favouriteCardsElements = favourites.map(createFavouriteCardElement);
-
-  appendElementsToContainer(mainCardsContainer, mainCardsElements);
 
   appendElementsToContainer(favouritesContainer, favouriteCardsElements);
 
@@ -38,4 +83,8 @@ document.addEventListener("DOMContentLoaded", async function () {
   const toggleFavouritesButton = document.getElementById("toggle-favourites");
 
   toggleFavouritesButton.addEventListener("click", toggleFavourites);
+
+  const searchField = document.getElementById("search-field");
+
+  searchField.addEventListener("input", debounce(searchEventHandler));
 });
