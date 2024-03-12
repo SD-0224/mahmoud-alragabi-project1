876d4d95d6b19f32bb4c2d7b1559475f1e417552
MedoAngel <actionman14.ma@gmail.com>
2024-03-12 21:32:41 +0100

    add TopBar component to header components

---
+++ src/components/header/components/top-bar/TopBar.jsx
@@ -0,0 +1,29 @@
+import styles from "./TopBar.module.css";
+
+export const TopBar = function ({ siteTitle, buttons = [] }) {
+  return (
+    <nav aria-label="Main Navigation">
+      <div className={styles["top-bar"]}>
+        <h1 className={styles["branding"]}>
+          <a href="/" aria-label="Homepage">
+            {siteTitle}
+          </a>
+        </h1>
+
+        <div className={styles["top-toggles"]}>
+          {buttons.map((button, idx) => (
+            <button
+              key={idx}
+              className={styles[button.className]}
+              aria-label={button.ariaLabel}
+              aria-pressed="false"
+            >
+              <ion-icon name={button.icon}></ion-icon>
+              <span className={styles["button-label"]}>{button.text}</span>
+            </button>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+};
