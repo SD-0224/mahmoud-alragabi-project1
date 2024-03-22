3989a2a5566e60585e3197f3dbff3b5bbcfc0f3a
MedoAngel <actionman14.ma@gmail.com>
2024-03-22 22:05:10 +0100

    use relative links for images, move assets folder to public

--- src/components/card/components/card-header/CardHeader.jsx
+++ src/components/card/components/card-header/CardHeader.jsx
@@ -3,7 +3,7 @@ import styles from "./CardHeader.module.css";
 export const CardHeader = function ({ cardImage }) {
   return (
     <div className={styles["card-header"]}>
-      <img src={require(`../../../../assets/images/logos/${cardImage}`)} />
+      <img src={`/assets/images/logos/${cardImage}`} />
     </div>
   );
 };
