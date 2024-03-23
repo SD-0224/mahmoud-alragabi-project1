56ab459a679e1abeab8a089d198d6b19205c16cc
MedoAngel <actionman14.ma@gmail.com>
2024-03-23 18:23:07 +0100

    add getAllTopics to http module

--- src/modules/http.js
+++ src/modules/http.js
@@ -5,3 +5,10 @@ export const fetchTopics = async function (endPoint) {
 
   return topics;
 };
+
+export const getAllTopics = async () => {
+  const topicsEndpoint = "https://tap-web-1.herokuapp.com/topics/list";
+  const allTopics = await fetchTopics(topicsEndpoint);
+
+  return allTopics;
+};
